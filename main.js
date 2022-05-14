4bf6efb8d5fc61ceb15ca6c7edecad565fb38c29
esraayaz <essayaz@hotmail.com>
2022-05-15 01:31:05 +0200

    Many bugs fixed

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-function formatDate(date) {
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
     let hours = date.getHours();
     if (hours < 10) {
         hours = `0${hours}`;
@@ -18,12 +19,23 @@ function formatDate(date) {
         "Friday",
         "Saturday",
     ];
-    let day = days[dayIndex];
+    let day = days[date.getDay()];
 
     return `${day} ${hours}:${minutes}`;
 }
 
 function displayWeatherCondition(response) {
+    celsiusTemperature = response.data.main.temp;
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute(
+        "src",
+        `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
     document.querySelector("#city").innerHTML = response.data.name;
     document.querySelector("#temperature").innerHTML = Math.round(
         response.data.main.temp
@@ -53,8 +65,6 @@ function handleSubmit(event) {
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
-searchCity("Hamburg");
-
 function searchLocation(position) {
     let apiKey = "df1e7d566653ff630e34dbe6521e4121";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
@@ -69,26 +79,25 @@ function getCurrentLocation(event) {
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
+searchCity("Hamburg");
+
 function convertToFahrenheit(event) {
     event.preventDefault();
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = "";
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
 function convertToCelsius(event) {
     event.preventDefault();
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = "";
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
+let celsiusTemperature = null;
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
-
-// sun-icon : <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sun icon" id="sun-icon" /> //
